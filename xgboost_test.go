401c509da4e87e3d0fe59869cf846044d5a36124
Jaakko Lukkari <jaakko@unity3d.com>
2018-08-17 15:47:16 +0300

    Example

--- xgboost_test.go
+++ xgboost_test.go
@@ -1,6 +1,7 @@
 package xgboost
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"math"
@@ -125,4 +126,53 @@ func TestXGBoost(t *testing.T) {
 	if totalDiff > 6.0 {
 		t.Error("error is too large")
 	}
+}
+
+func ExampleXGBoost() {
+	// create the train data
+	cols := 3
+	rows := 5
+	trainData := make([]float32, cols*rows)
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			trainData[(i*cols)+j] = float32((i + 1) * (j + 1))
+		}
+	}
+
+	trainLabels := make([]float32, rows)
+	for i := 0; i < rows; i++ {
+		trainLabels[i] = float32(1 + i*i*i)
+	}
+
+	matrix, _ := XGDMatrixCreateFromMat(trainData, rows, cols, -1)
+	matrix.SetFloatInfo("label", trainLabels)
+
+	booster, _ := XGBoosterCreate([]*XGDMatrix{matrix})
+
+	booster.SetParam("booster", "gbtree")
+	booster.SetParam("objective", "reg:linear")
+	booster.SetParam("max_depth", "5")
+	booster.SetParam("eta", "0.1")
+	booster.SetParam("min_child_weight", "1")
+	booster.SetParam("subsample", "0.5")
+	booster.SetParam("colsample_bytree", "1")
+	booster.SetParam("num_parallel_tree", "1")
+
+	// perform 200 learning iterations
+	for iter := 0; iter < 200; iter++ {
+		booster.UpdateOneIter(iter, matrix)
+	}
+
+	testData := make([]float32, cols*rows)
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			testData[(i*cols)+j] = float32((i + 1) * (j + 1))
+		}
+	}
+
+	testmat, _ := XGDMatrixCreateFromMat(testData, rows, cols, -1)
+	res, _ := booster.Predict(testmat, 0, 0)
+
+	fmt.Printf("%+v\n", res)
+	// output: [1.08002 2.5686886 7.86032 29.923136 63.76062]
 }
